142ea057522d283c87f2a2dab5d7de8c11c33c7e
hiepvg1999 <hiepbkcntt@gmail.com>
2021-05-20 08:31:47 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: "AIzaSyDqDwDwfzNVXG6zPrSeNqlN8bnFf3K9BMU",
+  authDomain: "fir-sample-2202a.firebaseapp.com",
+  projectId: "fir-sample-2202a",
+  storageBucket: "fir-sample-2202a.appspot.com",
+  messagingSenderId: "21041679879",
+  appId: "1:21041679879:web:6874239abfaf18b0b6e4a2"
+};
+firebase.initializeApp(firebaseConfig);
